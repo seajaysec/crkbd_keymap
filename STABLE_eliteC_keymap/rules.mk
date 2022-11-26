6cb7bd2447dedd0afab5a395e6e31b6ce85f2561
seajaysec <7016191+seajaysec@users.noreply.github.com>
2022-11-26 07:20:26 +0000

    Nov 25, 2022, 11:20 PM

---
+++ STABLE_eliteC_keymap/rules.mk
@@ -0,0 +1,12 @@
+# For Elite-C
+SRC += features/caps_word.c
+
+BOOTLOADER = atmel-dfu
+
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+SPLIT_KEYBOARD = yes
